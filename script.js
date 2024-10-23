a4db5497bc13e85279d240e5e0908fb25d4e1de4
TanuSingh1106 <tanusingh11062005@gmail.com>
2024-10-23 23:32:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let a=document.querySelector(".cross")
+a.addEventListener("click",()=>{
+   let main=document.querySelector(".main")
+    main.remove()
+})
+
+window.addEventListener("DOMContentLoaded",()=>{
+setTimeout(()=>{
+let cont=document.querySelector(".cont")
+cont.style.display="block"
+},5000)
+})
